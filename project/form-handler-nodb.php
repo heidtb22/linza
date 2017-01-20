99621d1f8bbc0ad12e067c5f2bf433dc1675ee54
heidtb22 <francescolinza@gmail.com>
2017-01-20 16:23:21 +0100

    Add files via upload

---
+++ project/form-handler-nodb.php
@@ -0,0 +1,21 @@
+<?php
+// Emails form data to you and the person submitting the form
+// This version requires no database.
+// Set your email below
+$myemail = "linzafrancesco@aol.de"; // Replace with your email, please
+
+// Receive and sanitize input
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+// set up email
+echo 'hello';
+$msg = "New contact form submission!\nName: " . $name . "\nEmail: " . $email . "\nPhone: " . $phone . "\nEmail: " . $email;
+$msg = wordwrap($msg,70);
+mail($myemail,"New Form Submission",$msg);
+mail($email,"Thank you for your form submission",$msg);
+echo 'Thank you for your submission.  Please <a href="index.html">Click here to return to our homepage.';
+
+?>
